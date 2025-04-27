d2ec70410be671d8195e7bd14ccfab4c295ba111
mehdinaji <seyedmehdinaji@gmail.com>
2025-04-27 22:32:08 +0330

    add search input

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,3 +16,21 @@ Route::post('/form', [WelcomeController::class, 'handleForm']);
 use App\Http\Controllers\TodoController;
 Route::get('/todos', [TodoController::class, 'showList']);
 Route::get('/todos/{id}', [TodoController::class, 'show']);
+
+Route::get('/search', function (Request $request) {
+    $query = $request->input('q');
+
+    if (!$query) {
+        return response()->json([
+            'message' => 'لطفا یک عبارت جستجو وارد کنید.'
+        ], 400);
+    }
+
+    $items = json_decode(file_get_contents(storage_path('items.json')), true) ?? [];
+
+    $results = array_filter($items, function ($item) use ($query) {
+        return stripos($item['title'], $query) !== false;
+    });
+
+    return response()->json(array_values($results));
+});
